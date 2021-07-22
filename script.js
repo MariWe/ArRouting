517f5274465bb409d96e61c63889165d031a8081
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-07-22 09:59:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 window.onload = () => {
 
+    getLocation();
+    test();
+
+    setInterval(function() { 
+        Pointing(); 
+    }, 100);
+}
+
+function getLocation(){
+    navigator.geolocation.watchPosition(function (position){
+        var lati = position.coords.latitude;
+        var long = position.coords.longitude;
+        console.log(lati, long);
+    })
+}
+
 function Pointing(){
     var pfeil = document.querySelector('#pfeil');
     var pony = document.querySelector('#pony');
@@ -7,7 +23,12 @@ function Pointing(){
 
     pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
 }  
-setInterval(function() { 
-    Pointing(); 
-}, 100);
+
+function test(){
+    var camera = document.querySelector('#camera');
+    var test = document.querySelector('#test');
+
+    if(camera.object3D.position == test.object3D.position){
+        pfeil.setAttribute(scale, "2 2 2");
+    }
 }
