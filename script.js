b85db8d18719237979399a3a79fac712e477796d
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-07-23 12:14:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ var pointOne = document.querySelector('#pointOne');
 var pointTwo = document.querySelector('#pointTwo');
 var pointThree = document.querySelector('#pointThree');
 var pointFour = document.querySelector('#pointFour');
-var one = {
+    var one = {
         latitude: 50.822163, 
         longitude: 12.939683
+    };
+    var two = {
+        latitude: 50.822597, 
+        longitude: 12.938641
+    };
+    var three = {
+        latitude: 50.823076, 
+        longitude: 12.937690
+    };
+    var four = {
+        latitude: 50.823420, 
+        longitude: 12.937663
     };
 
 window.onload = () => {
@@ -29,11 +41,36 @@ function getLocation(){
 function Pointing(){ 
     var pfeil = document.querySelector('#pfeil');
     var pony = document.querySelector('#pony');
+    var pointOne = document.querySelector('#pointOne');
+    var pointTwo = document.querySelector('#pointTwo');
+    var pointThree = document.querySelector('#pointThree');
+    var pointFour = document.querySelector('#pointFour');
 
     if(one.latitude != aktuell.latitude){
-    var position = pony.object3D.position;
+    var position = pointOne.object3D.position;
     pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z)); 
     }
     else{
+        if(two.latitude != aktuell.latitude){
+            var position = pointTwo.object3D.position;
+            pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
+        }
+        else{
+            if(three.latitude != aktuell.latitude){
+                var position = pointThree.object3D.position;
+                pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
+            }
+            else{
+                if(four.latitude != aktuell.latitude){
+                    var position = pointFour.object3D.position;
+                    pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
+                }
+                else{
+                    alert("You reached your goal!");
+                    var position = pony.object3D.position;
+                    pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
+                }
+            }
+        }
     }
 }  
