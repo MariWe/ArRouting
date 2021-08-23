f3cc73c1e770266c5664a4938b22e8d4945c2bfe
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-08-23 09:55:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
     }, 100)
 
         function Navigation(){
-             if(chP1 == false && d < 10){
+             if(chP1 == false && d < 5){
                 console.log("1");
                 one = document.querySelector('#two');
                 lat2 = 50.82256970090178;
