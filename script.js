848a13ddb4bb5d5e19bf7a3e1b42d4d45d604f0c
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-09-22 13:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,67 @@
-    let lat1, lat2, lon1, lon2, p1Lat, p1Lon, p2Lat, p2Lon, p3Lat, p3Lon, zLat, zLon;
-    let chP1 = false; 
-    let chP2 = false; 
-    let chP3 = false; 
-    let zielP = false; 
-    let d;
-    let help = 0;
-    var one;
+let next, lat1, lat2, lon1, lon2, one, d;
+window.onload = () => {
 
-    window.onload = () => {
-
-    getLocation();
     one = document.querySelector('#one');
-    var interval = setInterval(function(){
-            Pointing();
-    }, 100)
-    setInterval(function(){
-            Navigation();
-    }, 2000)
+    getLocation();
+    Display();
 
-        function Navigation(){
-             if(chP1 == false && d < 5){
-                console.log("1");
-                one = document.querySelector('#two');
-                lat2 = p2Lat;
-                lon2 = p2Lon;
-                chP1 = true;
-                document.getElementById("one").setAttribute('scale', '0, 0 ,0');
-                d = Infinity;
+    //"Navigation"
+    function Navigation() {
+        if (one !== null && next !== undefined) {
+            next = document.getElementById(one.dataset.next);
+            lat2 = parseFloat(next.dataset.lat);
+            lon2 = parseFloat(next.dataset.lon);
+            d = Distanz(lat1, lon1, lat2, lon2);
+            if (d < 5) {
+                if (next.dataset.next === "null") {
+                    //Ziel erreicht
                 }
-                if(chP2 == false && chP1 == true && d < 5){
-                    console.log("2");
-                    one = document.querySelector('#three');
-                    lat2 = p3Lat;
-                    lon2 = p3Lon;
-                    chP2 = true;
-                    document.getElementById("two").setAttribute('scale', '0, 0 ,0');
-                    d = Infinity;
+                else {
+                    next.setAttribute('scale, 0, 0, 0');
+                    one = next;
                 }
-                if(chP3 == false && chP1 == true && chP2 == true && d < 5){
-                    console.log("3");
-                    one = document.querySelector('#pony');
-                    lat2 = zLat;
-                    lon2 = zLon;
-                    chP3 = true;
-                    document.getElementById("three").setAttribute('scale', '0, 0 ,0');
-                    d = Infinity;
-                }
-                if(chP1 == true && chP2 == true && chP3 == true &&  zielP == false && d < 3){
-                    console.log('4');
-                    alert("Sie haben Ihr Ziel erreicht!");
-                    //clearInterval(interval);
-                } }
-               
-     //Marker Positionen
-     lat2 = 50.822218;
-     lon2 = 12.939607;
-     p2Lat = 50.822577;
-     p2Lon = 12.938716;
-     p3Lat = 50.823061;
-     p3Lon = 12.937573;
-     zLat = 50.823451;
-     zLon = 12.937632;
+            }
+        }
+    }
+
+    function Display(d) {
+        const div = document.querySelector('#demo');
+        div.innerText = "Distanz bis zum nächsten Punkt: " + d.toFixed(2);
+    }
 
     //Aktuelle Position
-    function getLocation(){
-        navigator.geolocation.watchPosition(function (position){
+    function getLocation() {
+        navigator.geolocation.watchPosition(function (position) {
             aktuell = position.coords;
             lat1 = aktuell.latitude;
             lon1 = aktuell.longitude;
-            Distanz();
-        })}
-        
+            one = document.querySelector('#one');
+            Navigation();
+            Pointing();
+            //Display();
+        })
+    }
 
     //Ausrichtung des Pfeils
-    function Pointing(){
+    function Pointing() {
         var pfeil = document.querySelector('#pfeil');
         var position = one.object3D.position;
         pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
     }
-    
+
     //distanzBerechnung
     //cr: "https://www.movable-type.co.uk/scripts/latlong.html"
-    function Distanz(){
-    const R = 6371e3; // metres
-    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-    const φ2 = lat2 * Math.PI/180;
-    const Δφ = (lat2-lat1) * Math.PI/180;
-    const Δλ = (lon2-lon1) * Math.PI/180;
-    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+    function Distanz(lat1, lat2, lon1, lon2) {
+        const R = 6371e3; // metres
+        const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+        const φ2 = lat2 * Math.PI / 180;
+        const Δφ = (lat2 - lat1) * Math.PI / 180;
+        const Δλ = (lon2 - lon1) * Math.PI / 180;
+        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
             Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    d = R * c; // in metres
-    const div = document.querySelector('#demo');
-    div.innerText = "Distanz bis zum nächsten Punkt: " + d.toFixed(2);
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        d = R * c; // in metres
+        return d;
     }
 }
-    
-    
