97fc1d8742b275eec9a9491921ab2a83e8ce299f
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-10-12 10:12:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ window.onload = () => {
     function createElement() {
         model = document.createElement('a-box');
         document.querySelector('a-scene').appendChild(model);
-        model.setAttribute('material', 'color: maroon; roughness: 1.0; metalness: 0.5;');
+        model.setAttribute('material', 'color: maroon; roughness: 0.4; metalness: 1.0;');
         model.setAttribute('id', 'box');
         model.setAttribute('scale', '0.75 0.75 0.75');
         model.setAttribute('gps-entity-place', 'latitude: ${lat1}; longitude: ${lon1};');
